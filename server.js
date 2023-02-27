ade2f50737de29ed8d95ad1b4ada2f71601f468c
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-02-27 10:18:00 -0500

    add questions

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+const questions = [
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'do',
+        choices: ['View All Employees', 'Add Employees', 'Update Employee Role', 'View All Roles', 'Add Roles', 'View All Departments', 'Add Departments', 'Quit', 'View All Employees']
+    },
+];
+
+function init() {
+    inquirer
+  .prompt(questions)
+  .then((response) => {
+    console.log(response);
+  }
+  );
+}
+
+init();
