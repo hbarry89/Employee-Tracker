03eb75ca96ea706022631d1143fa992d80db2a62
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-02-28 16:37:51 -0500

    edit seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,3 @@
-INSERT INTO department (name)
-VALUES ("Department 1"),
-       ("Department 2"),
-       ("Department 3");
-
-INSERT INTO role (name, salary, department_id)
-VALUES ("Role 1", 50000, 1),
-       ("Role 2", 60000, 2),
-       ("Role 3", 70000, 3);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Elliot", "Smith", 1, 3),
-       ("Amira", "Afzal", 2, 3),
-       ("Christoper", "Lee", 3, 3);
+INSERT INTO dempartment (name) VALUES (?), name
+INSERT INTO role (name, salary, department_id) VALUES (?, ?, ?), name, salary, department_id
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?), first_name, last_name, role_id, manager_id
