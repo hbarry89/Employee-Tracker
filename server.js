5977d2e6b05ec5917bc9aee1b71fd06fb5299760
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-01 21:50:19 -0500

    show correct manager values

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ function viewRole() {
 
 // View Employees
 function viewEmp() {
-  db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.first_name as manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.id = manager.id', function (err, results) {
+  db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager.first_name AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;', function (err, results) {
     console.table(results);
     mainQ();
   });
