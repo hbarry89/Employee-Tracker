b053e066fb3f30f8bc2bcdaef53a1518275740d4
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-03-01 21:59:33 -0500

    add manager last name

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ function viewRole() {
 
 // View Employees
 function viewEmp() {
-  db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager.first_name AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;', function (err, results) {
+  db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;', function (err, results) {
     console.table(results);
     mainQ();
   });
